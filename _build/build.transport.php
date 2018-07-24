a0eb4947ab953d1befb8df4e0f0e18305493c635
Mark Hamstra <hello@markhamstra.com>
2018-07-24 15:21:21 +0200

    Build for modx.com

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -23,7 +23,7 @@ if (!defined('MOREPROVIDER_BUILD')) {
     /* define version */
     define('PKG_NAME','SiteDashClient');
     define('PKG_NAME_LOWER',strtolower(PKG_NAME));
-    define('PKG_VERSION','0.5.0');
+    define('PKG_VERSION','0.6.0');
     define('PKG_RELEASE','pl');
 
     /* load modx */
