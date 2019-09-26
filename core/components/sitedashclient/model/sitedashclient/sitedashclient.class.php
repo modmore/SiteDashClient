e6604e04cfa54de13fe706387e6cb1971d5f14eb
Mark Hamstra <hello@markhamstra.com>
2019-09-26 14:13:36 +0200

    1.2.0

--- core/components/sitedashclient/model/sitedashclient/sitedashclient.class.php
+++ core/components/sitedashclient/model/sitedashclient/sitedashclient.class.php
@@ -7,7 +7,7 @@ require_once dirname(dirname(__DIR__)) . '/vendor/autoload.php';
  */
 class SiteDashClient
 {
-    const VERSION = '1.1.2-pl';
+    const VERSION = '1.2.0-pl';
     /**
      * @var modX|null $modx
      */
