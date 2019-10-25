bc10d7a1136ca2b8a623df09b27ba2b1c3f0aba7
Mark Hamstra <hello@markhamstra.com>
2019-10-26 00:52:36 +0200

    Check for proper session GC and table sizes

--- core/components/sitedashclient/src/Refresh.php
+++ core/components/sitedashclient/src/Refresh.php
@@ -149,6 +149,17 @@ class Refresh implements CommandInterface {
             $health['session_table'] = json_encode($i);
         }
 
+        $c = 'SELECT TABLE_ROWS, DATA_LENGTH, INDEX_LENGTH FROM information_schema.TABLES WHERE table_schema = ' . $this->modx->quote($this->modx->connection->config['dbname']) . ' AND table_name = ' . $this->modx->quote(trim($name, '`'));
+        $health['_debug'] = $c;
+        if ($sizeQuery = $this->modx->query($c)) {
+            $rows = $sizeQuery->fetchAll(\PDO::FETCH_ASSOC);
+            $health['session_sizes'] = reset($rows);
+        }
+
+        if ($lastQuery = $this->modx->query('SELECT access FROM ' . $name . ' ORDER BY access ASC LIMIT 1')) {
+            $health['session_oldest'] = $lastQuery->fetchColumn();
+        }
+
         return $health;
     }
 }
