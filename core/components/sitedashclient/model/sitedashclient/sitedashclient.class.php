5bd55d1cbf8b72d36032a2629420c0afec7805b0
Mark Hamstra <hello@markhamstra.com>
2019-11-25 15:27:14 +0100

    Bump version constant

--- core/components/sitedashclient/model/sitedashclient/sitedashclient.class.php
+++ core/components/sitedashclient/model/sitedashclient/sitedashclient.class.php
@@ -7,7 +7,7 @@ require_once dirname(dirname(__DIR__)) . '/vendor/autoload.php';
  */
 class SiteDashClient
 {
-    const VERSION = '1.3.0-pl';
+    const VERSION = '1.3.1-pl';
     /**
      * @var modX|null $modx
      */
