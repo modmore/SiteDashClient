52c2f694e449a83f89392fd4bdf8c25dedfdadb8
Mark Hamstra <hello@markhamstra.com>
2024-12-03 22:32:28 +0100

    Release long fixed issue

--- core/components/sitedashclient/model/sitedashclient/sitedashclient.class.php
+++ core/components/sitedashclient/model/sitedashclient/sitedashclient.class.php
@@ -7,7 +7,7 @@ require_once dirname(dirname(__DIR__)) . '/vendor/autoload.php';
  */
 class SiteDashClient
 {
-    const VERSION = '1.7.0-pl';
+    const VERSION = '1.7.1-pl';
     /**
      * @var modX|null $modx
      */
