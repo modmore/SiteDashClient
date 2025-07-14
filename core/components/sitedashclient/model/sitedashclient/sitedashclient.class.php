a63b499a37b9d2027730bf775e294ef72f9229b7
Mark Hamstra <hello@markhamstra.com>
2025-07-14 20:40:15 +0200

    bump client version with minor fixes

--- core/components/sitedashclient/model/sitedashclient/sitedashclient.class.php
+++ core/components/sitedashclient/model/sitedashclient/sitedashclient.class.php
@@ -7,7 +7,7 @@ require_once dirname(dirname(__DIR__)) . '/vendor/autoload.php';
  */
 class SiteDashClient
 {
-    const VERSION = '1.7.1-pl';
+    const VERSION = '1.7.2-pl';
     /**
      * @var modX|null $modx
      */
